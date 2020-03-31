a61b957c61758d0add0223fe57da8a9a82b56504
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-03-31 12:26:22 -0500

    added prometheus example

---
+++ prometheus/lclr-metrics.php
@@ -0,0 +1,28 @@
+<?php
+
+// Endpoint: https://web.lcrc.anl.gov/public/waggle/private/training_data/aot_audio_and_images/image_bottom/metrics.php
+// Description: Serves training size metrics.
+// 
+
+printf("# HELP waggle_training_data_total Number of items in training data.\n");
+printf("# TYPE waggle_training_data_total gauge\n");
+
+printf("# HELP waggle_training_data_bytes Total bytes of training data.\n");
+printf("# TYPE waggle_training_data_bytes gauge\n");
+
+$resource = "image_bottom";
+
+foreach (glob("*/") as $nodedir) {
+    $nodeID = trim($nodedir, "/");
+    $total = 0;
+    $bytes = 0;
+
+    foreach (glob($nodedir . "*.jpg") as $filename) {
+        $total += 1;
+        $bytes += filesize($filename);
+    }
+
+    printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $total);
+    printf("waggle_training_data_bytes{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $bytes);
+}
+?>
