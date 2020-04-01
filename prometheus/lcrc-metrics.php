cde7c715398a1f800422fc399cf183f21970d17f
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-01 10:01:57 -0500

    use for multiple resources

--- prometheus/lcrc-metrics.php
+++ prometheus/lcrc-metrics.php
@@ -1,6 +1,6 @@
 <?php
 
-// Endpoint: https://web.lcrc.anl.gov/public/waggle/private/training_data/aot_audio_and_images/image_bottom/metrics.php
+// Endpoint: https://web.lcrc.anl.gov/public/waggle/private/training_data/aot_audio_and_images/metrics.php
 // Description: Serves training size metrics.
 // 
 
@@ -9,23 +9,26 @@ printf("# TYPE waggle_training_data_total gauge\n");
 // printf("# HELP waggle_training_data_bytes Total bytes of training data.\n");
 // printf("# TYPE waggle_training_data_bytes gauge\n");
 
-$resource = "image_bottom";
+function printResourceMetrics($resource) {
+    foreach (new FilesystemIterator($resource) as $dir) {
+        if (!$dir->isDir()) {
+            continue;
+        }
 
-foreach (new FilesystemIterator(".") as $dir) {
-    if (!$dir->isDir()) {
-        continue;
+        $nodeID = $dir->getBasename();
+        $total = 0;
+        // $bytes = 0;
+
+        foreach (new FilesystemIterator($dir) as $file) {
+            $total += 1;
+            // $bytes += $file->getSize();
+        }
+
+        printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $total);
+        // printf("waggle_training_data_bytes{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $bytes);
     }
-
-    $nodeID = $dir->getBasename();
-    $total = 0;
-    // $bytes = 0;
-
-    foreach (new FilesystemIterator($dir) as $file) {
-        $total += 1;
-        // $bytes += $file->getSize();
-    }
-
-    printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $total);
-    // printf("waggle_training_data_bytes{node_id=\"%s\",resource=\"%s\"} %d\n", $nodeID, $resource, $bytes);
 }
+
+printResourceMetrics("image_bottom");
+printResourceMetrics("image_top");
 ?>
