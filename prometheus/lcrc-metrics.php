dc0fd3427c52c2094f584d0cfa737fe5713bb5e8
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-07 08:57:11 -0500

    added support for new directory structure. added support for error class.

--- prometheus/lcrc-metrics.php
+++ prometheus/lcrc-metrics.php
@@ -7,7 +7,10 @@
 printf("# HELP waggle_training_data_total Number of items in training data.\n");
 printf("# TYPE waggle_training_data_total gauge\n");
 
-function printResourceMetrics($basedir, $resource, $class) {
+// Having to match patterns like:
+// aot_audio_and_images/image_bottom/001e061182bb/2020-04-07T02:40:22+00:00.jpg
+// ----- basedir -------------------/---nodeID---/*.jpg
+function printResourceMetrics1($basedir, $resource, $class) {
     foreach (new FilesystemIterator($basedir) as $dir) {
         if (!$dir->isDir()) {
             continue;
@@ -20,7 +23,27 @@ function printResourceMetrics($basedir, $resource, $class) {
     }
 }
 
-printResourceMetrics("aot_audio_and_images/image_bottom", "image_bottom", "good");
-printResourceMetrics("aot_audio_and_images/bad_image_bottom", "image_bottom", "bad");
-printResourceMetrics("aot_audio_and_images/image_top", "image_top", "good");
+// Having to match patterns like:
+// aot_audio_and_images/good/001e061182bb/image/bottom/2020/04/07/2020-04-07T02:40:22+00:00.jpg
+// ----- basedir -----------/---nodeID---/---subdir---/YYYY/MM/DD/*.jpg
+function printResourceMetrics2($basedir, $subdir, $resource, $class) {
+    foreach (new FilesystemIterator($basedir) as $dir) {
+        if (!$dir->isDir()) {
+            continue;
+        }
+
+        $nodeID = $dir->getBasename();
+        $total = (new GlobIterator($dir . "/" . $subdir . "/*/*/*/*.jpg"))->count();
+
+        printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\",class=\"%s\"} %d\n", $nodeID, $resource, $class, $total);
+    }
+}
+
+printResourceMetrics1("aot_audio_and_images/image_bottom", "image_bottom", "good");
+printResourceMetrics1("aot_audio_and_images/bad_image_bottom", "image_bottom", "bad");
+printResourceMetrics1("aot_audio_and_images/image_top", "image_top", "good");
+
+printResourceMetrics2("aot_audio_and_images/good", "image/bottom", "image_bottom", "good");
+printResourceMetrics2("aot_audio_and_images/bad", "image/bottom", "image_bottom", "bad");
+printResourceMetrics2("aot_audio_and_images/error", "image/bottom", "image_bottom", "error");
 ?>
