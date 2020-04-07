415b85b80010862a3a20ab729c25a703bfc22ce0
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-07 12:00:14 -0500

    fixed extensions

--- prometheus/lcrc-metrics.php
+++ prometheus/lcrc-metrics.php
@@ -7,7 +7,7 @@
 printf("# HELP waggle_training_data_total Number of items in training data.\n");
 printf("# TYPE waggle_training_data_total gauge\n");
 
-function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class) {
+function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class, $ext) {
     $totals = [];
 
     // match directories like:
@@ -20,7 +20,7 @@ function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class) 
             }
 
             $nodeID = $dir->getBasename();
-            $total = (new GlobIterator($dir . "/*.*"))->count();
+            $total = (new GlobIterator($dir . "/" . $ext))->count();
 
             if (!isset($totals[$nodeID])) {
                 $totals[$nodeID] = 0;
@@ -41,7 +41,7 @@ function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class) 
             }
 
             $nodeID = $dir->getBasename();
-            $total = (new GlobIterator($dir . "/" . $subdir . "/*/*/*/*.*"))->count();
+            $total = (new GlobIterator($dir . "/" . $subdir . "/*/*/*/" . $ext))->count();
 
             if (!isset($totals[$nodeID])) {
                 $totals[$nodeID] = 0;
@@ -57,23 +57,23 @@ function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class) 
     }
 }
 
-printResourceMetrics("aot_audio_and_images/image_bottom", "aot_audio_and_images/good", "image/bottom", "image_bottom", "good");
-printResourceMetrics("aot_audio_and_images/bad_image_bottom", "aot_audio_and_images/bad", "image/bottom", "image_bottom", "bad");
-printResourceMetrics("aot_audio_and_images/error_image_bottom", "aot_audio_and_images/error", "image/bottom", "image_bottom", "error");
+printResourceMetrics("aot_audio_and_images/image_bottom", "aot_audio_and_images/good", "image/bottom", "image_bottom", "good", "*.jpg");
+printResourceMetrics("aot_audio_and_images/bad_image_bottom", "aot_audio_and_images/bad", "image/bottom", "image_bottom", "bad", "*.jpg");
+printResourceMetrics("aot_audio_and_images/error_image_bottom", "aot_audio_and_images/error", "image/bottom", "image_bottom", "error", "*.jpg");
 
-printResourceMetrics("aot_audio_and_images/image_top", "aot_audio_and_images/good", "image/top", "image_top", "good");
-printResourceMetrics("aot_audio_and_images/bad_image_top", "aot_audio_and_images/bad", "image/top", "image_top", "bad");
-printResourceMetrics("aot_audio_and_images/error_image_top", "aot_audio_and_images/error", "image/top", "image_top", "error");
+printResourceMetrics("aot_audio_and_images/image_top", "aot_audio_and_images/good", "image/top", "image_top", "good", "*.jpg");
+printResourceMetrics("aot_audio_and_images/bad_image_top", "aot_audio_and_images/bad", "image/top", "image_top", "bad", "*.jpg");
+printResourceMetrics("aot_audio_and_images/error_image_top", "aot_audio_and_images/error", "image/top", "image_top", "error", "*.jpg");
 
-printResourceMetrics("aot_audio_and_images/audio_microphone", "aot_audio_and_images/good", "audio/microphone", "audio_microphone", "good");
-printResourceMetrics("aot_audio_and_images/bad_audio_microphone", "aot_audio_and_images/bad", "audio/microphone", "audio_microphone", "bad");
-printResourceMetrics("aot_audio_and_images/error_audio_microphone", "aot_audio_and_images/error", "audio/microphone", "audio_microphone", "error");
+printResourceMetrics("aot_audio_and_images/audio_microphone", "aot_audio_and_images/good", "audio/microphone", "audio_microphone", "good", "*.mp3");
+printResourceMetrics("aot_audio_and_images/bad_audio_microphone", "aot_audio_and_images/bad", "audio/microphone", "audio_microphone", "bad", "*.mp3");
+printResourceMetrics("aot_audio_and_images/error_audio_microphone", "aot_audio_and_images/error", "audio/microphone", "audio_microphone", "error", "*.mp3");
 
-printResourceMetrics("aot_audio_and_images/video_bottom", "aot_audio_and_images/good", "video/bottom", "video_bottom", "good");
-printResourceMetrics("aot_audio_and_images/bad_video_bottom", "aot_audio_and_images/bad", "video/bottom", "video_bottom", "bad");
-printResourceMetrics("aot_audio_and_images/error_video_bottom", "aot_audio_and_images/error", "video/bottom", "video_bottom", "error");
+printResourceMetrics("aot_audio_and_images/video_bottom", "aot_audio_and_images/good", "video/bottom", "video_bottom", "good", "*.mp4");
+printResourceMetrics("aot_audio_and_images/bad_video_bottom", "aot_audio_and_images/bad", "video/bottom", "video_bottom", "bad", "*.mp4");
+printResourceMetrics("aot_audio_and_images/error_video_bottom", "aot_audio_and_images/error", "video/bottom", "video_bottom", "error", "*.mp4");
 
-printResourceMetrics("aot_audio_and_images/video_top", "aot_audio_and_images/good", "video/top", "video_top", "good");
-printResourceMetrics("aot_audio_and_images/bad_video_top", "aot_audio_and_images/bad", "video/top", "video_top", "bad");
-printResourceMetrics("aot_audio_and_images/error_video_top", "aot_audio_and_images/error", "video/top", "video_top", "error");
+printResourceMetrics("aot_audio_and_images/video_top", "aot_audio_and_images/good", "video/top", "video_top", "good", "*.mp4");
+printResourceMetrics("aot_audio_and_images/bad_video_top", "aot_audio_and_images/bad", "video/top", "video_top", "bad", "*.mp4");
+printResourceMetrics("aot_audio_and_images/error_video_top", "aot_audio_and_images/error", "video/top", "video_top", "error", "*.mp4");
 ?>
