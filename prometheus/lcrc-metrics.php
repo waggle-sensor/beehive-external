56e1baeca253b1964ae59554e615ab8539107008
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-04-07 09:25:50 -0500

    fixed totals

--- prometheus/lcrc-metrics.php
+++ prometheus/lcrc-metrics.php
@@ -7,43 +7,61 @@
 printf("# HELP waggle_training_data_total Number of items in training data.\n");
 printf("# TYPE waggle_training_data_total gauge\n");
 
-// Having to match patterns like:
-// aot_audio_and_images/image_bottom/001e061182bb/2020-04-07T02:40:22+00:00.jpg
-// ----- basedir -------------------/---nodeID---/*.jpg
-function printResourceMetrics1($basedir, $resource, $class) {
-    foreach (new FilesystemIterator($basedir) as $dir) {
-        if (!$dir->isDir()) {
-            continue;
+function printResourceMetrics($basedir1, $basedir2, $subdir, $resource, $class) {
+    $totals = [];
+
+    // match directories like:
+    // aot_audio_and_images/image_bottom/001e061182bb/2020-04-07T02:40:22+00:00.jpg
+    // ----- basedir -------------------/---nodeID---/*.jpg
+    try {
+        foreach (new FilesystemIterator($basedir1) as $dir) {
+            if (!$dir->isDir()) {
+                continue;
+            }
+
+            $nodeID = $dir->getBasename();
+            $total = (new GlobIterator($dir . "/*.jpg"))->count();
+
+            if (!isset($totals[$nodeID])) {
+                $totals[$nodeID] = 0;
+            }
+
+            $totals[$nodeID] += $total;
         }
+    } catch (Exception $e) {
+    }
 
-        $nodeID = $dir->getBasename();
-        $total = (new GlobIterator($dir . "/*.jpg"))->count();
+    // match directories like:
+    // aot_audio_and_images/good/001e061182bb/image/bottom/2020/04/07/2020-04-07T02:40:22+00:00.jpg
+    // ----- basedir -----------/---nodeID---/---subdir---/YYYY/MM/DD/*.jpg
+    try {
+        foreach (new FilesystemIterator($basedir2) as $dir) {
+            if (!$dir->isDir()) {
+                continue;
+            }
 
+            $nodeID = $dir->getBasename();
+            $total = (new GlobIterator($dir . "/" . $subdir . "/*/*/*/*.jpg"))->count();
+
+            if (!isset($totals[$nodeID])) {
+                $totals[$nodeID] = 0;
+            }
+
+            $totals[$nodeID] += $total;
+        }
+    } catch (Exception $e) {
+    }
+
+    foreach ($totals as $nodeID => $total) {
         printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\",class=\"%s\"} %d\n", $nodeID, $resource, $class, $total);
     }
 }
 
-// Having to match patterns like:
-// aot_audio_and_images/good/001e061182bb/image/bottom/2020/04/07/2020-04-07T02:40:22+00:00.jpg
-// ----- basedir -----------/---nodeID---/---subdir---/YYYY/MM/DD/*.jpg
-function printResourceMetrics2($basedir, $subdir, $resource, $class) {
-    foreach (new FilesystemIterator($basedir) as $dir) {
-        if (!$dir->isDir()) {
-            continue;
-        }
+printResourceMetrics("aot_audio_and_images/image_bottom", "aot_audio_and_images/good", "image/bottom", "image_bottom", "good");
+printResourceMetrics("aot_audio_and_images/bad_image_bottom", "aot_audio_and_images/bad", "image/bottom", "image_bottom", "bad");
+printResourceMetrics("aot_audio_and_images/error_image_bottom", "aot_audio_and_images/error", "image/bottom", "image_bottom", "error");
 
-        $nodeID = $dir->getBasename();
-        $total = (new GlobIterator($dir . "/" . $subdir . "/*/*/*/*.jpg"))->count();
-
-        printf("waggle_training_data_total{node_id=\"%s\",resource=\"%s\",class=\"%s\"} %d\n", $nodeID, $resource, $class, $total);
-    }
-}
-
-printResourceMetrics1("aot_audio_and_images/image_bottom", "image_bottom", "good");
-printResourceMetrics1("aot_audio_and_images/bad_image_bottom", "image_bottom", "bad");
-printResourceMetrics1("aot_audio_and_images/image_top", "image_top", "good");
-
-printResourceMetrics2("aot_audio_and_images/good", "image/bottom", "image_bottom", "good");
-printResourceMetrics2("aot_audio_and_images/bad", "image/bottom", "image_bottom", "bad");
-printResourceMetrics2("aot_audio_and_images/error", "image/bottom", "image_bottom", "error");
+printResourceMetrics("aot_audio_and_images/image_top", "aot_audio_and_images/good", "image/top", "image_top", "good");
+printResourceMetrics("aot_audio_and_images/bad_image_top", "aot_audio_and_images/bad", "image/top", "image_top", "bad");
+printResourceMetrics("aot_audio_and_images/error_image_top", "aot_audio_and_images/error", "image/top", "image_top", "error");
 ?>
